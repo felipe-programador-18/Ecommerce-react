4eeac129cf099351ad3d52af36a6bf6f1441d0e2
Felipe <machadofelipe2016@outlook.com>
2022-07-29 21:02:47 -0300

    c4-rearrange my routes and adding outlet

--- test/src/App.js
+++ test/src/App.js
@@ -1,10 +1,28 @@
 import "./directory/directory.styles.scss"
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home.component";
+import NotFoundHere from "./routes/notfound";
+import Navigation from "./routes/navigation/navigation";
+
+
+
+
+const MyShop = () => {
+  return <h1>I am shop page</h1>
+  
+}
+
+
+
 const App = () => {
  return(<>
   <Routes>
-   <Route path="/"  element={<Home/>} />
+   <Route path="/"  element={<Navigation/>} >
+    <Route path="*" element={<NotFoundHere/> }  />
+    <Route index element={<Home/>} />
+    <Route path="shop"  element={<MyShop/>}  />
+   
+   </Route>
   </Routes>
  
  </>)
