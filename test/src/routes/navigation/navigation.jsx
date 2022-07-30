4e38db70831036d6c9939dd59862ac38658eb8d0
Felipe <machadofelipe2016@outlook.com>
2022-07-30 13:25:20 -0300

    c12-adding several together package here

--- test/src/routes/navigation/navigation.jsx
+++ test/src/routes/navigation/navigation.jsx
@@ -1,12 +1,23 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 import { Fragment } from "react"
+import { ReactComponent as CrownLogo } from "../../assets/crown (1).svg"
+import "./navigation.styles.scss"
+
 
 const Navigation = () => {
     return ( <Fragment >
         <div className="navigation" >  
-        <h1>Navigation here!!</h1>
-        <Outlet/>
+         <Link  className="logo-container" to='/' >       
+         <CrownLogo className="logo"  />
+
+         </Link>
+          <div className="nav-links-container" >
+           <Link className="nav-link" to='/shop' >Shop</Link>
+          </div>
+
         </div>
+       
+        <Outlet/>
         </Fragment>
       )
   }
