4deffd4b64eea4e7fdc2d6e95677aa20db42afcd
Felipe <machadofelipe2016@outlook.com>
2022-08-27 12:24:41 -0300

    c26-adding bootstrap inside index

--- test/src/index.js
+++ test/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import  'bootstrap/dist/css/bootstrap.css'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
