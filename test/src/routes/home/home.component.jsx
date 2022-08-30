de77ebed5bb9190d5cb35a623f72930ddeb16b91
Felipe <machadofelipe2016@outlook.com>
2022-08-30 20:27:57 -0300

    c36-move on folder inside component

--- test/src/routes/home/home.component.jsx
+++ test/src/routes/home/home.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Directory from '../../directory/directory.component'
+import Directory from '../../component/directory/directory.component'
 import { Outlet } from 'react-router-dom'
 
 
